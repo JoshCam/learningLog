9b2fce69ceea3c8d5bc8edc0f3ce61578104a1e3
JoshCam <joshcampden15@gmail.com>
2021-07-21 12:29:51 +0100

    Added procfile and requirements

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3chutb=&a==bqbp9c8*zssn31fxt$le)s(dfse$ds#@17o=sm*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
